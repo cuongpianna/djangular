a902f77ca6acba49584e8f3558eb57bd4eaa3ba6
cuongpianna <cuongpianna1996@gmail.com>
2018-06-26 17:40:19 +0700

    ss

--- users/serializers.py
+++ users/serializers.py
@@ -2,7 +2,41 @@ from rest_framework import serializers
 
 from .models import Profile, User
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('url','username','email')
+        fields = ('username','email','password')
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+    username = serializers.CharField()
+    password = serializers.CharField()
+    password2 = serializers.CharField()
+
+    class Meta:
+        model = User
+        fields = ('email','username','password','password2')
+
+    def validate_email(self,email):
+        existing = User.objects.filter(email = email).first()
+        if existing:
+            raise serializers.ValidationError('Someone with that email address has already registered. Was it you?')
+        return email
+
+    def validate(self, data):
+        if not data.get('password') or not data.get('password2'):
+            raise serializers.ValidationError("please enter a password and confirm it")
+
+        if data.get('password') != data.get('password2'):
+            raise serializers.ValidationError("Those passwords don't match.")
+        return data
+
+    def create(self, validated_data):
+        user = User(
+            email = validated_data.get('email'),
+            username = validated_data.get('username')
+        )
+        user.set_password(validated_data.get('password'))
+        user.save()
+        return user
+
--- users/views.py
+++ users/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework import viewsets
+from rest_framework.decorators import api_view,permission_classes,action
+from rest_framework.permissions import AllowAny
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer,UserRegisterSerializer
 from .models import User
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @action(methods=['post'],permission_classes=[AllowAny,],detail=False)
+    def register(self,request):
+        serializer = UserRegisterSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+
+        model_serializer = UserSerializer(data=serializer.data)
+        model_serializer.is_valid(raise_exception=True)
+        model_serializer.save()
+        return Response(model_serializer.data)
 
 
 def index(request):
